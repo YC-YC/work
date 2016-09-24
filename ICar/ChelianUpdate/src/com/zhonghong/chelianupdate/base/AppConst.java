657d6265501928572305c40c1404c0dd53055246
YC-YC <yellowc.yc@aliyun.com>
2016-09-24 17:14:03 +0800

    适配开发环境与生产环境

--- ICar/ChelianUpdate/src/com/zhonghong/chelianupdate/base/AppConst.java
+++ ICar/ChelianUpdate/src/com/zhonghong/chelianupdate/base/AppConst.java
@@ -10,7 +10,10 @@ public class AppConst {
 	public static final String UPDATE_INFO_FILE_NAME="/updateInfo.txt";
 	public static final String URL_SOURCE_PART="update-center/getUpdateInfoByVin";
 	public static final String URL_REPORT_UPDATE_STATUS="update-center/reportUpdateStatus";
-	public static final String URL_HOST="http://cowinmguat.timasync.com/";
-	
+//	public static final String URL_HOST="http://cowinmguat.timasync.com/";
+	/**生产环境*/
+	public static final String URL_HOST_PRODUCT = "http://cowinmg.timasync.com/";
+	/**测试环境*/
+	public static final String URL_HOST_TEST = "http://cowinmguat.timasync.com/";
 	
 }
